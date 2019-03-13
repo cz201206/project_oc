fee653f9ae52697a11cc0cab74dfc12a0e712878
cz <774666549@qq.com>
2019-03-13 14:12:55 +0800

    Initial Commit

---
+++ project_oc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  project_oc
+//
+//  Created by cz on 2019/3/13.
+//  Copyright © 2019 cz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
